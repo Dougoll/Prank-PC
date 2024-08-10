026c689da2c298d0688bd628863832071288133d
Dougoll <65680164+Dougoll@users.noreply.github.com>
2024-08-09 20:02:32 -0400

    Update _UNINSTALL.bat

--- BADASS_LATEST-PC/dependencies/_UNINSTALL.bat
+++ BADASS_LATEST-PC/dependencies/_UNINSTALL.bat
@@ -1,3 +1,3 @@
-powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "C:\Users\%username%\Appdata\Local\Drivers\data2.ps1"
+powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file "C:\Users\%username%\Appdata\Local\Drivers\dependencies\data2.ps1"
 rmdir C:\Users\%username%\Appdata\Local\Drivers /Q /S
 exit
